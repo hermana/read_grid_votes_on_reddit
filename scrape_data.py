fb5df3477aea835bf0af4c8c4470197663e4be87
hermana <aeherman@ualberta.ca>
2022-06-27 23:24:34 -0600

    fixmes

--- scrape_data.py
+++ scrape_data.py
@@ -5,29 +5,29 @@ import votes
 import re
 import praw
 
-# votes = {}
-
-grid_votes = votes.Votes()
-
-reddit = praw.Reddit(client_id=personal_config.CLIENT_ID, client_secret=personal_config.CLIENT_SECRET, user_agent=personal_config.AGENT)
-
-# TODO: Make an interface to customize this using a url instead.
-post_with_grid_votes = reddit.submission("v2l22i")
-comments = post_with_grid_votes.comments
-# FIXME: does this throw some comments away? 
-comments.replace_more(limit=0)
 
 def flip(word):
     first = word[0]
     last = word[1]
     return last + first
 
+
 def preprocess(comment):
-    #replace punctuation with spaces
-    comment = re.sub(r"[,.;@#?!&$]+\ *", " ", comment)
-    #make all upper case
+    comment = re.sub(r"[,.;@#?!&$-/]+\ *", " ", comment)
     return comment.upper()
 
+def get_url():
+    return input('Please enter the url of the grid on reddit: ')
+
+grid_votes = votes.Votes()
+reddit = praw.Reddit(client_id=personal_config.CLIENT_ID, client_secret=personal_config.CLIENT_SECRET, user_agent=personal_config.AGENT)
+url = get_url()
+post_with_grid_votes = reddit.submission(url=url)
+comments = post_with_grid_votes.comments
+comments.replace_more(limit=None)
+
+
+# FIXME: lambda?
 for comment in comments:
     processed_comment = preprocess(comment.body)
     comment_text = processed_comment.split()

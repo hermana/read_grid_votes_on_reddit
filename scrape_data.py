6dd531ffe5042d9fba9b6453a54f918bd7516597
hermana <aeherman@ualberta.ca>
2022-06-27 23:11:29 -0600

    votes class

--- scrape_data.py
+++ scrape_data.py
@@ -5,7 +5,9 @@ import votes
 import re
 import praw
 
-votes = {}
+# votes = {}
+
+grid_votes = votes.Votes()
 
 reddit = praw.Reddit(client_id=personal_config.CLIENT_ID, client_secret=personal_config.CLIENT_SECRET, user_agent=personal_config.AGENT)
 
@@ -26,23 +28,14 @@ def preprocess(comment):
     #make all upper case
     return comment.upper()
 
-def add_vote(vote:str):
-    if vote in votes:
-        votes.update({vote: votes[vote]+1})
-    else:
-        votes[vote] = 1
-
 for comment in comments:
     processed_comment = preprocess(comment.body)
     comment_text = processed_comment.split()
     for word in comment_text:
         if grid_vote_regex.forwards.match(word):
-            # add the item to the dictionary and increment its count.
-            # TOD0: trim any commas or punctuation
-            add_vote(word)
+            grid_votes.add_vote(word)
         elif grid_vote_regex.backwards.match(word):
-            # flip it and do the same thing. 
             word = flip(word)
-            add_vote(word)
+            grid_votes.add_vote(word)
             
-print(votes)
+print(grid_votes.votes)
--- votes.py
+++ votes.py
@@ -1,2 +1,10 @@
-# TODO: make this a static class
+class Votes:
+    votes = {}
+    
+    def add_vote(self, vote:str):
+        if vote in self.votes:
+            self.votes.update({vote: self.votes[vote]+1})
+        else:
+            self.votes[vote] = 1
 
+    
